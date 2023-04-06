eff4727e082256102cc788f08f145576e81b0b04
satwikkamath <107809929+satwikkamath@users.noreply.github.com>
2023-04-06 14:27:23 +0530

    Updated user appointment page

--- index.js
+++ index.js
@@ -34,47 +34,53 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String,
     phoneNumber: Number,
-    age:Number,
-    gender:String,
+    age: Number,
+    gender: String,
 })
 
-const Doctor = mongoose.model("doctor",docSchema);
-const User = mongoose.model("user",userSchema);
+const appointmentSchema = new mongoose.Schema({
+    doctorName: String,
+    userName: String,
+    // prescription: String
+})
+
+const Doctor = mongoose.model("doctor", docSchema);
+const User = mongoose.model("user", userSchema);
+const Appointment = mongoose.model("appointment", appointmentSchema);
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
-app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set("view engine", "ejs");
 
 
-let citizenName  
+let citizenName
 
 // Main Page (home route)
-app.get("/",function(req,res){
-    // res.render("userMainPage",{doctorData:[]});
-    // res.render("userSignup",{error:false})
+app.get("/", function (req, res) {
+
     res.render("mainPage")
 })
 
 //citizen login through first page
-app.get("/citizenLogin",function(req,res){
-    res.render("userLogin",{text:false, passwordFail:false});
+app.get("/citizenLogin", function (req, res) {
+    res.render("userLogin", { text: false, passwordFail: false });
 })
 
 // citizen Sign Up through first page
-app.get("/citizenSignup",function(req,res){
-    res.render("userSignup",{error:false});
+app.get("/citizenSignup", function (req, res) {
+    res.render("userSignup", { error: false });
 })
 
 //doctor login through first page
-app.get("/doctor",function(req,res){
-    res.render("docLogin",{passwordFail:false});
+app.get("/doctor", function (req, res) {
+    res.render("docLogin", { passwordFail: false });
 })
 
 
 //User Sign Up
 
-app.post("/userSignUp",function(req,res){
+app.post("/userSignUp", function (req, res) {
     const receivedName = req.body.name;
     const receivedAge = req.body.age;
     const receivedEmail = req.body.email;
@@ -82,31 +88,27 @@ app.post("/userSignUp",function(req,res){
     const receivedPhno = req.body.phno;
     const receivedPswd = req.body.password;
     const cpswd = req.body.cpassword;
-    if(receivedPswd != cpswd)
-    {
-        res.render("userSignup",{error:true});  // if password and confirm password wont match
-        
+    if (receivedPswd != cpswd) {
+        res.render("userSignup", { error: true });  // if password and confirm password wont match
+
     }
-    else
-    {   
-        User.findOne({email:receivedEmail}).then(function(data){
-            if(data)
-            {
-                res.render("userLogin",{text: "Account already exists with this Email",passwordFail:false});     // if an account already exists while signing up
+    else {
+        User.findOne({ email: receivedEmail }).then(function (data) {
+            if (data) {
+                res.render("userLogin", { text: "Account already exists with this Email", passwordFail: false });     // if an account already exists while signing up
 
             }
-            else
-            {
+            else {
                 const user = new User({
                     name: receivedName,
                     email: receivedEmail,                // if no account exists, new account is created 
                     password: receivedPswd,
                     phoneNumber: receivedPhno,
-                    age:receivedAge,
-                    gender:receivedGender,
+                    age: receivedAge,
+                    gender: receivedGender,
                 })
                 user.save();
-                res.render("userLogin",{text: "Your account was succesfully created",passwordFail:false});
+                res.render("userLogin", { text: "Your account was succesfully created", passwordFail: false });
 
             }
 
@@ -117,48 +119,86 @@ app.post("/userSignUp",function(req,res){
 
 //User Login
 
-app.post("/userLogin",function(req,res){
+app.post("/userLogin", function (req, res) {
     const email = req.body.email;
     const pswd = req.body.password;
-    User.findOne({email:email}).then(function(user){
+    User.findOne({ email: email }).then(function (user) {
         citizenName = user.name;
-        if(pswd===user.password){
-            res.render("userMainPage",{doctorData:false,userName:user.name});  // if password matched
+        if (pswd === user.password) {
+            res.render("userMainPage", { doctorName: false, doctorData: false, userName: user.name });  // if password matched
         }
-        else{
-            res.render("userLogin",{passwordFail:true,text:false});  // if password not matched
+        else {
+            res.render("userLogin", { passwordFail: true, text: false });  // if password not matched
         }
     })
 })
 
 //Doctor Login
 
-app.post("/docLogin",function(req,res){
+app.post("/docLogin", function (req, res) {
     const email = req.body.email;
     const pswd = req.body.password;
-    Doctor.findOne({email:email}).then(function(user){
+    Doctor.findOne({ email: email }).then(function (user) {
         doctorName = user.name;
-        if(pswd===user.password){
-            res.render("doctorMainPage",{userName:doctorName});  // if password matched
+        if (pswd === user.password) {
+            // res.render("doctorMainPage",{userName:doctorName});  // if password matched
+            Appointment.findOne({ doctorName: doctorName }).then(function (data) {
+                res.render("doctorMainPage", { userName: doctorName, patientName: data.userName });
+                // res.render("userMainPage", { doctorName: doctorName, doctorData: false, userName: citizenName });
+            });
         }
-        else{
-            res.render("docLogin",{passwordFail:true});  // if password not matched
+        else {
+            res.render("docLogin", { passwordFail: true });  // if password not matched
         }
     })
+
 })
 
 // doctor search
-app.post("/doctorSearch",function(req,res){
+app.post("/doctorSearch", function (req, res) {
     const receivedArea = req.body.area;
     const receivedSpecialization = req.body.specialization;
-    Doctor.find({area: receivedArea, specialization:receivedSpecialization}).then(function(doctors){
+    Doctor.find({ area: receivedArea, specialization: receivedSpecialization }).then(function (doctors) {
         // console.log(doctors);
-        res.render("userMainPage",{doctorData:doctors,userName:citizenName});
+        res.render("userMainPage", {doctorName: false, doctorData: doctors, userName: citizenName });
         // res.redirect("/");
     });
 })
 
 
-app.listen(3000,function(){
+// Scheduling Appointment
+
+app.post("/scheduleAppointment",function (req, res) {
+
+    const receivedDocName = req.body.doctorName;
+
+    const appointments = new Appointment({
+        doctorName: receivedDocName,
+        userName: citizenName,
+
+    });
+    appointments.save();
+    
+    Appointment.find({userName:citizenName }).then(function (data) {
+
+        res.render("userAppointments", { doctors: data, userName: citizenName });
+    });
+  
+
+
+
+
+});
+
+
+app.get("/myAppointments", function(req,res){
+    Appointment.find({userName:citizenName }).then(function (data) {
+
+        res.render("userAppointments", { doctors: data, userName: citizenName });
+    });
+})
+
+
+app.listen(3000, function () {
     console.log("Server running on port 3000");
 })
